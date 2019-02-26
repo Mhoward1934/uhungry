ffbc5a22240766b5759b17be1975e4347dcb5e2a
Katie Higgins <katherine9513@gmail.com>
2019-02-26 14:06:20 -0600

    building front end routes

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function NavBar() {
+
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand" href="/">uHungry?</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+                    aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                        <a className="nav-item nav-link" href="/">Search</a>
+                        <a className="nav-item nav-link" href="/list">List</a>
+                    </div>
+                </div>
+            </nav>
+
+        </div>
+    );
+};
+
+export default NavBar
